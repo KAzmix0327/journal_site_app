0e90ca0b789ccbeef5c4ec99f05d18c3fb4189ad
KAzumix0327 <yamauchi138@icloud.com>
2022-09-11 11:53:53 +0900

    articlesのindex作成

--- journal_site_app/app/Http/Controllers/ArticleController.php
+++ journal_site_app/app/Http/Controllers/ArticleController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\article;
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -15,7 +15,7 @@ class ArticleController extends Controller
     public function index()
     {
         $articles = Article::all();
-        return view();
+        return view('articles.index', ['articles' => $articles]);
     }
 
     /**
